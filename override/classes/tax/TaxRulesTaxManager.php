b77cdfd3c6fb7c62adead868e6b4b6123b9f8200
Greg Tam <gtam@arrow.com>
2016-10-12 13:37:04 -0700

    latest from prestashop

--- override/classes/tax/TaxRulesTaxManager.php
+++ override/classes/tax/TaxRulesTaxManager.php
@@ -198,39 +198,65 @@ class TaxRulesTaxManager extends TaxRulesTaxManagerCore implements TaxManagerInt
 
 	private function getDefaultTaxCalculator($postcode=0, $taxes=array())
 	{
-		$cache_id = (int)$this->address->id_country.'-'.(int)$this->address->id_state.'-'.$postcode.'-'.(int)$this->type;
-		if (!Cache::isStored($cache_id))
-		{
-			$rows = Db::getInstance()->executeS('
-			SELECT *
-			FROM `'._DB_PREFIX_.'tax_rule`
-			WHERE `id_country` = '.(int)$this->address->id_country.'
-			AND `id_tax_rules_group` = '.(int)$this->type.'
-			AND `id_state` IN (0, '.(int)$this->address->id_state.')
-			AND (\''.pSQL($postcode).'\' BETWEEN `zipcode_from` AND `zipcode_to` OR (`zipcode_to` = 0 AND `zipcode_from` IN(0, \''.pSQL($postcode).'\')))
-			ORDER BY `zipcode_from` DESC, `zipcode_to` DESC, `id_state` DESC, `id_country` DESC');
+		static $tax_enabled = null;
 
-			$behavior = 0;
-			$first_row = true;
+        if (isset($this->tax_calculator)) {
+            return $this->tax_calculator;
+        }
 
-			foreach ($rows as $row)
-			{
-				$tax = new Tax((int)$row['id_tax']);
+        if ($tax_enabled === null) {
+            $tax_enabled = $this->configurationManager->get('PS_TAX');
+        }
 
-				$taxes[] = $tax;
+        if (!$tax_enabled) {
+            return new TaxCalculator(array());
+        }
 
-				// the applied behavior correspond to the most specific rules
-				if ($first_row)
-				{
-					$behavior = $row['behavior'];
-					$first_row = false;
-				}
+        $taxes = array();
+        $postcode = 0;
 
-				if ($row['behavior'] == 0)
-					 break;
-			}
-			Cache::store($cache_id, new TaxCalculator($taxes, $behavior));
-		}
-		return Cache::retrieve($cache_id);
+        if (!empty($this->address->postcode)) {
+            $postcode = $this->address->postcode;
+        }
+
+        $cache_id = (int)$this->address->id_country.'-'.(int)$this->address->id_state.'-'.$postcode.'-'.(int)$this->type;
+
+        if (!Cache::isStored($cache_id)) {
+            $rows = Db::getInstance()->executeS('
+				SELECT tr.*
+				FROM `'._DB_PREFIX_.'tax_rule` tr
+				JOIN `'._DB_PREFIX_.'tax_rules_group` trg ON (tr.`id_tax_rules_group` = trg.`id_tax_rules_group`)
+				WHERE trg.`active` = 1
+				AND tr.`id_country` = '.(int)$this->address->id_country.'
+				AND tr.`id_tax_rules_group` = '.(int)$this->type.'
+				AND tr.`id_state` IN (0, '.(int)$this->address->id_state.')
+				AND (\''.pSQL($postcode).'\' BETWEEN tr.`zipcode_from` AND tr.`zipcode_to`
+					OR (tr.`zipcode_to` = 0 AND tr.`zipcode_from` IN(0, \''.pSQL($postcode).'\')))
+				ORDER BY tr.`zipcode_from` DESC, tr.`zipcode_to` DESC, tr.`id_state` DESC, tr.`id_country` DESC');
+
+            $behavior = 0;
+            $first_row = true;
+
+            foreach ($rows as $row) {
+                $tax = new Tax((int)$row['id_tax']);
+
+                $taxes[] = $tax;
+
+                // the applied behavior correspond to the most specific rules
+                if ($first_row) {
+                    $behavior = $row['behavior'];
+                    $first_row = false;
+                }
+
+                if ($row['behavior'] == 0) {
+                    break;
+                }
+            }
+            $result = new TaxCalculator($taxes, $behavior);
+            Cache::store($cache_id, $result);
+            return $result;
+        }
+
+        return Cache::retrieve($cache_id);
 	}
 }
